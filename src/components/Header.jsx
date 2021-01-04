4d10599d2375f858459ad8e0e293dfee45d2fc47
Daniel <luis.mendoza@olozfera.com>
2021-01-04 00:44:08 -0600

    links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 
@@ -6,7 +7,11 @@ import "../assets/styles/components/header.scss";
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src={Logo} alt="Platzi Video" />
+
+        <Link to="/">
+            <img className="header__img" src={Logo} alt="Platzi Video" />
+        </Link>
+
         <div className="header__menu">
             <div className="header__menu--profile">
                 <img src={userIcon} alt="Profile" />
@@ -14,7 +19,11 @@ const Header = () => (
             </div>
             <ul>
                 <li><a href="/">Cuenta</a></li>
-                <li><a href="/">Cerrar Sesión</a></li>
+                <li>
+                    <Link to="login">
+                        Iniciar sesión
+                    </Link>
+                </li>
             </ul>
         </div>
     </header>
